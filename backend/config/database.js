299ec8c55ddc378d8e5aad3dca42e110f8bda6b8
Roffin <roffin@techgentsia.com>
2023-05-01 10:11:09 +0530

    feat: env added for db config

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,14 +3,16 @@ import mysql from "mysql2";
 // create the connection to database
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "db_restaurant"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "db_restaurant",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-db.connect(error => {
+db.connect((error) => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
 });
