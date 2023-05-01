71397b71f7d6bcf1703717591c42194ca3eff258
Roffin <roffin@techgentsia.com>
2023-05-01 13:50:51 +0530

    fix: allow hosts

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 // const path = require('path');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: "all",
+  },
 });
-
